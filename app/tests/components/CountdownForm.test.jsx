244298117637a7bea1e1af00647cd226da7cf6f4
chandu <chandrasr.palle@gmail.com>
2018-01-22 23:55:08 +0100

    test case added for Countdownform

---
+++ app/tests/components/CountdownForm.test.jsx
@@ -0,0 +1,23 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM= require('react-dom');
+var TestUtils=require('react-addons-test-utils');
+var $= require('jQuery');
+
+var CountdownForm= require('CountdownForm');
+
+describe('CountdownForm',()=>{
+  it('should exist',()=>{
+    expect(CountdownForm).Exist;
+  });
+  it('should call onSetCountdown if valid seconds',()=>{
+  var spy=expect.createSpy();
+  var countdownForm=TestUtils.renderIntoDocument(<CountdownForm onSetCountDown={spy}/>);
+  var $el=$(ReactDOM.findDOMNode(countdownForm));
+
+  countdownForm.refs.seconds.value='109';
+  TestUtils.Simulate.submit($el.find('form')[0]);
+
+  expect(spy).toHaveBeenCalledWith(109);
+  });
+});
