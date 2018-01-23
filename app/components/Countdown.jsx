56511c71c73238262b030e5edaab863fc9426ffe
chandu <chandrasr.palle@gmail.com>
2018-01-24 00:49:21 +0100

    componentWillUnmount added to Countdown

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -28,6 +28,10 @@ var Countdown= React.createClass({
     }
 
   },
+  componentWillUnmount:function(){
+    clearInterval(this.timer);
+    this.timer= undefined;
+  },
   startTimer:function(){
     this.timer= setInterval(()=>{
       var newCount = this.state.count -1;
