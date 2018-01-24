aa88b9afdc4b179718cd4155e5df4d214d72f363
chandu <chandrasr.palle@gmail.com>
2018-01-24 16:58:49 +0100

    foundation settings

--- app/app.jsx
+++ app/app.jsx
@@ -6,7 +6,6 @@ var Timer=require('Timer');
 var Countdown= require('Countdown');
 
 //Load Foundation
-require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
 //App CSS adding custom styles
