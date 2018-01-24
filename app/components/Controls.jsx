c22a580621dc216eb2c93f547cecc07e693fe977
chandu <chandrasr.palle@gmail.com>
2018-01-24 02:47:21 +0100

    Timer component functionality added

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -14,7 +14,7 @@ var Controls= React.createClass({
         if(countdownStatus==='started'){
           return <button className="button secondary" onClick={this.onStatusChange("paused")}>Pause</button>
         }
-        else if(countdownStatus==='paused')
+        else
         {
           return <button className="button primary" onClick={this.onStatusChange("started")}>Start</button>
         }
--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,11 +1,56 @@
 var React= require('react');
+var Clock= require('Clock');
+var Controls= require('Controls');
 
-var Timer = () => {
-  return(
-    <div>
-      <h1>Time.jsx</h1>
-    </div>
-  );
-};
+var Timer = React.createClass({
+  getInitialState:function(){
+    return{
+      count:0,
+      timerStatus:"paused"
+    };
+  },
+  componentDidUpdate:function(prevProps,prevState){
+    if(this.state.timerStatus!== prevState.timerStatus)
+    {
+      switch(this.state.timerStatus){
+        case "started":
+        this.setTimer();
+        break;
+        case "stopped":
+        this.setState({count:0});
+        case "paused":
+        clearInterval(this.timer);
+        this.timer=undefined;
+        break;
+      }
+    }
+
+  },
+  componentWillUnmount:function(){
+    clearInterval(this.timer);
+  },
+  setTimer:function(){
+      this.timer=setInterval(()=>{
+      var newCount = this.state.count + 1;
+      this.setState({count:newCount>0? newCount : 0});
+      },
+      1000);
+  },
+  handleStatusChange:function(newStatus){
+    this.setState({
+      timerStatus : newStatus
+    });
+  },
+  render:function(){
+    var {count,timerStatus}= this.state;
+    return(
+      <div>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalseconds={count}/>
+        <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
+      </div>
+    );
+  }
+});
 
 module.exports=Timer;
